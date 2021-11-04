a3ed0c8eaae791709c13947c9d7c07dc62b09538
mello <falmelopros@gmail.com>
2021-11-04 09:16:22 +0300

    AuthControll UserModel and .env modification

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,46 +1,45 @@
-const userModel = require('../models/UserModel');
-const jwt = require('jsonwebtoken');
-const { signUpErrors, signInErrors } = require('../error/ErrorUtils');
+const userModel = require("../models/UserModel");
+const jwt = require("jsonwebtoken");
+const { signUpErrors, signInErrors } = require("../error/ErrorUtils");
 
 const maxAge = 3 * 24 * 60 * 60 * 1000;
 
 const createToken = (id) => {
-    return jwt.sign({ id }, process.env.TOKEN_SECRET, {
-        expiresIn: maxAge
-    })
+  return jwt.sign({ id }, process.env.TOKEN_SECRET, {
+    expiresIn: maxAge,
+  });
 };
 
 // création user
 module.exports.signUp = async (req, res) => {
-    const { pseudo, email, password } = req.body
-    try {
-        const user = await userModel.create({ pseudo, email, password })
-        res.status(201).json({ user: user._id })
-    }
-    catch (err) {
-        const errors = signUpErrors(err);
-        res.status(200).send({ errors })
-    }
-}
+  const { pseudo, email, password } = req.body;
+  try {
+    const user = await userModel.create({ pseudo, email, password });
+    res.status(201).json({ user: user._id });
+  } catch (err) {
+    const errors = signUpErrors(err);
+    res.status(200).send({ errors });
+  }
+};
 
 // connexion user
 module.exports.signIn = async (req, res) => {
-    const { email, password } = req.body
+  const { email, password } = req.body;
 
-    try {
-        const user = await userModel.login(email, password);
-        const token = createToken(user._id);
-        res.cookie('jwt', token, { httpOnly: true, maxAge });
-        res.status(200).json({ user: user._id })
-    } catch (err) {
-        const errors = signInErrors(err);
-        res.status(200).json({ errors });
-        res.status(200).json(err);
-    }
-}
+  try {
+    const user = await userModel.login(email, password);
+    const token = createToken(user._id);
+    res.cookie("jwt", token, { httpOnly: true, maxAge });
+    res.status(200).json({ user: user._id });
+  } catch (err) {
+    const errors = signInErrors(err);
+    res.status(200).json({ errors });
+    res.status(200).json(err);
+  }
+};
 
 // déconexion user
 module.exports.logOut = (req, res) => {
-    res.cookie('jwt', '', { maxAge: 1 });
-    res.redirect('/');
-}
+  res.cookie("jwt", "", { maxAge: 1 });
+  res.redirect("/");
+};
--- models/UserModel.js
+++ models/UserModel.js
@@ -1,76 +1,74 @@
-const mongoose = require('mongoose');
-const { isEmail } = require('validator');
-const bcrypt = require('bcrypt');
+const mongoose = require("mongoose");
+const { isEmail } = require("validator");
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema(
-    {
-        pseudo: {
-            type: String,
-            required: true,
-            minLength: 3,
-            maxLength: 55,
-            unique: true,
-            trim: true
-        },
-        email: {
-            type: String,
-            required: true,
-            validate: [isEmail],
-            lowerCase: true,
-            unique: true,
-            trim: true
-        },
-        password: {
-            type: String,
-            required: true,
-            max: 1024,
-            minlength: 6,
-        },
-        picture: {
-            type: String,
-            default: "./uploads/profil/"
-        },
-        bio: {
-            type: String,
-            max: 1024,
-        },
-        followers: {
-            type: [String]
-        },
-        following: {
-            type: [String]
-        },
-        likes: {
-            type: [String]
-        }
+  {
+    pseudo: {
+      type: String,
+      required: true,
+      minLength: 3,
+      maxLength: 55,
+      unique: true,
+      trim: true,
     },
-    {
-        timestamps: true,
-    }
+    email: {
+      type: String,
+      required: true,
+      validate: [isEmail],
+      lowerCase: true,
+      unique: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      max: 1024,
+      minlength: 6,
+    },
+    picture: {
+      type: String,
+      default: "./uploads/profil/",
+    },
+    bio: {
+      type: String,
+      max: 1024,
+    },
+    followers: {
+      type: [String],
+    },
+    following: {
+      type: [String],
+    },
+    likes: {
+      type: [String],
+    },
+  },
+  {
+    timestamps: true,
+  }
 );
 
 // jouer la fonction avant d'enregistrer dans l'affichage: 'block',
-userSchema.pre("save", async function(next) {
-    const salt = await bcrypt.genSalt();
-    this.password = await bcrypt.hash(this.password, salt);
-    next();
-  });
-  
-  // vérification du password poster avec le password du parametre
-  userSchema.statics.login = async function(email, password) {
-    const user = await this.findOne({ email });
-    if (user) {
-      const auth = await bcrypt.compare(password, user.password);
-      if (auth) {
-        return user;
-      }
-      throw Error('incorrect password');
+userSchema.pre("save", async function (next) {
+  const salt = await bcrypt.genSalt();
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+// vérification du password poster avec le password du parametre
+userSchema.statics.login = async function (email, password) {
+  const user = await this.findOne({ email });
+  if (user) {
+    const auth = await bcrypt.compare(password, user.password);
+    if (auth) {
+      return user;
     }
-    throw Error('incorrect email')
-  };
+    throw Error("incorrect password");
+  }
+  throw Error("incorrect email");
+};
 
-
-const UserModel = mongoose.model("user", userSchema);  // creer table(user) dans la BD
+const UserModel = mongoose.model("user", userSchema); // creer table(user) dans la BD
 
 module.exports = UserModel;
-
